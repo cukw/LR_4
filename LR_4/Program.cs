51cebf8c8f0b83eeec477980ad1d99eae5c5e442
cukw <kan142006@gmail.com>
2025-11-22 22:56:02 +0300

    Добавьте файлы проекта.

---
+++ LR_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    private static List<int> CreateList()
+    {
+        var r = new Random();
+        var list = new List<int>();
+        for(int i = 0; i < r.Next(5,10); i++)
+        {
+            list.Add(r.Next(1,10));
+        }
+        return list;
+    }
+
+    private static string DisplayList(List<int>list) => string.Join(',', list);
+
+
+    private static List<int> ChangeIndex(List<int> list)
+    {
+        (list[list.LastIndexOf(list.Max())], list[list.IndexOf(list.Min())]) = (list[list.IndexOf(list.Min())], list[list.LastIndexOf(list.Max())]);
+
+        return list;
+    }
+
+    static void Main()
+    {
+        var list = CreateList();
+
+        Console.WriteLine($"Massive: {DisplayList(list)}");
+
+        list = ChangeIndex(list);
+
+        Console.WriteLine($"Massive after change: {DisplayList(list)}");
+
+    }
+}
